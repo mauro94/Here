56ddf32fc8ef85af9b5f2c50230ef69ed7bdc03f
Mauro Amarante <mauro.amarante94@gmail.com>
2018-01-11 11:16:09 -0600

    Initial Commit

---
+++ Here/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Here
+//
+//  Created by Mauro Amarante Esparza on 1/11/18.
+//  Copyright © 2018 Mauro Amarante Esparza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
