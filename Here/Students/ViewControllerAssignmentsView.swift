855b7e1b45ee174db84cf2b20cbe5522a9678215
Mauro Amarante <mauro@Mauros-MacBook-Pro.local>
2018-02-15 11:28:18 -0600

    Added necessary files

---
+++ Here/Students/ViewControllerAssignmentsView.swift
@@ -0,0 +1,35 @@
+//
+//  ViewControllerAssignmentsView.swift
+//  Here
+//
+//  Created by Mauro Amarante Esparza on 2/15/18.
+//  Copyright © 2018 Mauro Amarante Esparza. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerAssignmentsView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
